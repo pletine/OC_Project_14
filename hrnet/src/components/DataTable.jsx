1123f49ce5e345e4ea4fac431b7ceba62c2c9dab
pletine <pierre.letinevez@gmail.com>
2023-10-18 16:12:35 +0400

    Tentatives pour améliorer le tableau

--- hrnet/src/components/DataTable.jsx
+++ hrnet/src/components/DataTable.jsx
@@ -1,10 +1,16 @@
 import "./dataTable.scss";
-import * as React from "react";
-import { useTable } from "react-table";
+import { useMemo } from "react";
+import {
+  useFilters,
+  useSortBy,
+  useTable,
+  usePagination,
+  useGlobalFilter,
+} from "react-table";
 
 export default function DataTable({ dataInput }) {
-  const data = React.useMemo(() => dataInput, [dataInput]);
-  const columns = React.useMemo(
+  const data = useMemo(() => dataInput, [dataInput]);
+  const columns = useMemo(
     () => [
       {
         Header: "First Name",
@@ -46,23 +52,68 @@ export default function DataTable({ dataInput }) {
     []
   );
 
-  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
-    useTable({ columns, data });
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    page,
+    prepareRow,
+    state: { pageIndex, pageSize, globalFilter },
+    setGlobalFilter,
+  } = useTable(
+    { columns, data, initialState: { pageIndex: 0, pageSize: 10 } },
+    useFilters,
+    useGlobalFilter,
+    useSortBy,
+    usePagination
+  );
+
+  const pageCount = page.length; // state.pageCount; // Obtenez pageCount depuis l'état
+  const onChangePageSize = (e) => {
+    setPageSize(Number(e.target.value));
+  };
+
+  const goToPage = (pageIndex) => {
+    // Utilisez setPage pour aller à la page souhaitée
+    setGlobalFilter("");
+  };
+
+  const previousPage = () => {
+    if (pageIndex > 0) {
+      goToPage(pageIndex - 1);
+    }
+  };
+
+  const nextPage = () => {
+    if (pageIndex < pageCount - 1) {
+      goToPage(pageIndex + 1);
+    }
+  };
 
   return (
     <div id="employee-table" className="display">
+      <input
+        type="text"
+        placeholder="Recherche"
+        // onChange={(e) => setGlobalFilter(e.target.value)}
+      />
       <table {...getTableProps()}>
         <thead>
           {headerGroups.map((headerGroups) => (
             <tr {...headerGroups.getHeaderGroupProps()}>
               {headerGroups.headers.map((column) => (
-                <th {...column.getHeaderProps()}>{column.render("Header")}</th>
+                <th {...column.getHeaderProps(column.getSortByToggleProps())}>
+                  {column.render("Header")}
+                  <span>
+                    {column.isSorted ? (column.isSortedDesc ? "▼" : "▲") : ""}
+                  </span>
+                </th>
               ))}
             </tr>
           ))}
         </thead>
         <tbody {...getTableBodyProps()}>
-          {rows.map((row) => {
+          {page.map((row) => {
             prepareRow(row);
             return (
               <tr {...row.getRowProps()}>
@@ -74,6 +125,23 @@ export default function DataTable({ dataInput }) {
           })}
         </tbody>
       </table>
+      <div>
+        <button onClick={() => previousPage()} disabled={pageIndex === 0}>
+          Précedent
+        </button>
+        <span>
+          Page{" "}
+          <strong>
+            {pageIndex + 1} / {pageOptions.length}
+          </strong>{" "}
+        </span>
+        <button
+          onClick={() => nextPage()}
+          disabled={pageIndex >= pageCount - 1}
+        >
+          Suivant
+        </button>
+      </div>
     </div>
   );
 }
